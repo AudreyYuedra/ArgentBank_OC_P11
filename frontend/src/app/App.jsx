086fe82b34be329cb65913eb6b6e8ca64bed1baf
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-04 01:23:35 +0100

    ajout routes + styled-components

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom"
+import Header from "../components/Header/Header.jsx"
+import Home from "../pages/Home.jsx"
+import SignIn from "../pages/SignIn.jsx"
+import User from "../pages/User.jsx"
+import Footer from "../components/Footer/Footer.jsx"
+import { createGlobalStyle } from "styled-components"
+
+export default App
+
+const GlobalStyle = createGlobalStyle`
+  div {
+    backgroung-color: ${colors.bg_primary};
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: ${colors.txt_primary};
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }`
+
+function App() {
+   return (
+      <GlobalStyle>
+         <Header />
+
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/User" element={<User />} />
+         </Routes>
+
+         <Footer />
+      </GlobalStyle>
+   )
+}
---
+++ frontend/src/utils/style/colors.jsx
@@ -0,0 +1,12 @@
+export default colors
+
+const colors = {
+   bg_primary: "#FFFFFF",
+   bg_secondary: "12002b",
+
+   txt_primary: "#2c3e50",
+   txt_secondary: "222222",
+
+   details_bg: "00bc77",
+   details_line: "cccccc",
+}
