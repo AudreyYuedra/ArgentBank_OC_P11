4d327e2bc862c5ec90da9a57283e7ff6e2b0afe5
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-04 05:40:57 +0100

    correction style Banner

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,43 +1,19 @@
 import img_banner from "../../assets/images/bank-tree.jpeg"
-import styled from "styled-components"
 
 export default Banner
 
-const BannerContainer = styled.div`
-   width: 100%;
-   height: 300px;
-`
-const ImgBanner = styled.img`
-   background-size: cover;
-   background-repeat: no-repeat;
-   position: relative;
-`
-const CardBanner = styled.div`
-   position: relative;
-   top: 2rem;
-   width: 200px;
-   padding: 2rem;
-   text-align: left;
-   margin: 0 auto;
-`
-const Subtitle = styled.p`
-   font-weight: bold;
-   font-size: 1rem;
-`
-const Text = styled.p`
-   font-size: 0.9rem;
-`
-
 function Banner() {
    return (
-      <BannerContainer>
-         <ImgBanner src={img_banner} />
-         <CardBanner>
-            <Subtitle>No fees.</Subtitle>
-            <Subtitle>No minimum deposit.</Subtitle>
-            <Subtitle>High interest rates.</Subtitle>
-            <Text>Open a savings account with Argent Bank today!</Text>
-         </CardBanner>
-      </BannerContainer>
+      <section className="section-banner">
+         <img src={img_banner} className="img-banner" />
+
+         <article className="content-banner">
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+
+            <span className="text">Open a savings account with Argent Bank today!</span>
+         </article>
+      </section>
    )
 }
