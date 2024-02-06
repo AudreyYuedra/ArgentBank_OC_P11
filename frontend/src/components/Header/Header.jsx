17785b83d2f21664fae82415fd9881a324c25aba
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-06 02:46:03 +0100

    mise en place déco user (prob parent)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import Link from "react-router-dom"
+import Link, { useNavigate } from "react-router-dom"
 import Logo from "../../assets/images/argentBankLogo.png"
 
 import styled from "styled-components"
 import mixins from "../../utils/style"
+import { useDispatch, useSelector } from "react-redux"
 
 export default Header
 
@@ -19,7 +20,7 @@ const LogoImg = styled.img`
       cursor: pointer;
    }
 `
-const StyledLink = styled(Link)`
+const StyledLink = styled.ul`
    width: 74px;
    height: 18px;
    font-weight: bold;
@@ -32,6 +33,19 @@ const StyledLink = styled(Link)`
 `
 
 function Header() {
+   const { user } = useSelector((state) => state.auth)
+   const { userName } = useSelector((state) => state.user)
+   const dispatch = useDispatch()
+   const navigate = useNavigate()
+
+   //* Déco user
+   const onLogout = () => {
+      dispatch(logout())
+		dispatch(reset())
+		dispatch(resetUser())
+		navigate('/')
+   }
+
    return (
       <Nav>
          <Link to="/">
@@ -39,9 +53,29 @@ function Header() {
                <LogoImg src={Logo} alt="Argent Bank" />
             </h1>
          </Link>
-         <StyledLink to="/SignIn">
-            <i class="fa fa-user-circle"></i>
-            Sign In
+
+         <StyledLink>
+            { user ?
+               <li>
+                  <Link to="/User">
+                     <i className="fa fa-user-circle"></i>
+                     {username}
+                  </Link>
+               </li>
+               <li>
+                 <Link to="/SignIn">
+                     <i className="fa fa-sign-out"></i>
+                     Sign Out
+                  </Link>
+               </li>
+               :
+               <li>
+                  <Link to="/SignIn">
+                     <i class="fa fa-user-circle"></i>
+                     Sign In
+                  </Link>
+               </li>
+            }
          </StyledLink>
       </Nav>
    )
